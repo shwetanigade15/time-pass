fdb1667389f3f38d6def5d2497066abadb0affec
shwetanigade15 <shwetanigade04031@gmail.com>
2024-11-12 14:55:26 +0530

    Update addition.py

--- add.py
+++ add.py
@@ -1,3 +1,10 @@
-x = 5
-y = 10
-print(x + y)
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
